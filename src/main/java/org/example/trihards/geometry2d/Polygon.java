2f6fbe144f1a63b47f9e6948b69449292078d6f2
cdmckinley <work.craigmckinley@gmail.com>
2022-10-30 15:48:11 -0500

    Added documentation for Polygon

--- src/main/java/org/example/trihards/geometry2d/Polygon.java
+++ src/main/java/org/example/trihards/geometry2d/Polygon.java
@@ -1,7 +1,21 @@
 package org.example.trihards.geometry2d;
 
+/**
+ * The interface for 2D polygons.
+ */
 public interface Polygon {
 
+    /**
+     * The perimeter of the polygon
+     *
+     * @return the perimeter
+     */
     int perimeter();
+
+    /**
+     * The area of the polygon
+     *
+     * @return the area
+     */
     int area();
 }
