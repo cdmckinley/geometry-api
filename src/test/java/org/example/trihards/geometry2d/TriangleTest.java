c93a10c63bbb93ad27c9ea57838cfb4c78bf157d
cdmckinley <work.craigmckinley@gmail.com>
2022-11-04 16:58:20 -0500

    Began writing the test for Triangle

---
+++ src/test/java/org/example/trihards/geometry2d/TriangleTest.java
@@ -0,0 +1,26 @@
+package org.example.trihards.geometry2d;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO JavaDocs
+public class TriangleTest {
+
+    @Test
+    public void testPerimeter() {
+        int sideA = 7;
+        int sideB = 8;
+        int sideC = 9;
+        // TODO create a triangle with those side lengths
+        assertEquals(24, 0); // replace 0 with the triangle's perimeter method
+    }
+
+    @Test
+    public void testArea() {
+        int height = 7;
+        int width = 4;
+        // TODO create a triangle with that height and width
+        assertEquals(14, 0); // replace 0 with triangle's area method
+    }
+}
