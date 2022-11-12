de69b1f65208c7de92a4d1d80b6dfe9e7251f012
cdmckinley <work.craigmckinley@gmail.com>
2022-11-12 12:41:39 -0600

    Now returns null when nothing is calculated for a formula

--- src/main/java/org/example/trihards/response/Response2d.java
+++ src/main/java/org/example/trihards/response/Response2d.java
@@ -3,22 +3,24 @@ package org.example.trihards.response;
 import org.example.trihards.geometry2d.Shape;
 
 public class Response2d {
-    private double perimeter;
-    private double area;
+    private Object perimeter;
+    private Object area;
 
-    public double getPerimeter() {
+    public Object getPerimeter() {
         return perimeter;
     }
 
     public void setPerimeter(Shape shape) {
-        this.perimeter = shape.perimeter();
+        double perimeter = shape.perimeter();
+        if (perimeter > 0.0) this.perimeter = perimeter;
     }
 
-    public double getArea() {
+    public Object getArea() {
         return area;
     }
 
     public void setArea(Shape shape) {
-        this.area = shape.area();
+        double area = shape.area();
+        if (area > 0.0) this.area = area;
     }
 }
