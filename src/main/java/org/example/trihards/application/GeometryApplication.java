b9a9f19b2746db009f4cd9098557dfb8b9424b7c
cdmckinley <work.craigmckinley@gmail.com>
2022-11-12 13:36:56 -0600

    Made JavaDocs

--- src/main/java/org/example/trihards/application/GeometryApplication.java
+++ src/main/java/org/example/trihards/application/GeometryApplication.java
@@ -10,11 +10,18 @@ import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * The Geometry web application.
+ */
 @ApplicationPath("/services")
 public class GeometryApplication extends Application {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
+    /**
+     * Gets the services to be used in the application
+     * @return a Set of services
+     */
     @Override
     public Set<Class<?>> getClasses() {
         Set<Class<?>> hashset = new HashSet<>();
